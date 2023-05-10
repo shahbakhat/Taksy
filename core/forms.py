6b7712a50adf83242694f33e5fd7e808762cb311
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-10 09:03:04 +0100

    created the sign-up form logic and implemented the logic return cleaned data rsther than uncleaned data

---
+++ core/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=250)
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
+
+    class Meta:
+        model = User
+    fields = ('emial', 'first_name', 'last_name', 'password1', 'password2')
+    # Funcion to return clean data in form if dictionary
+    # String and primary keys will be returned asobjects
+    def clean_field(self):
+        data = self.cleaned_data['email'].lowercase()
+        if User.objects.filter(email=data).exists:
+            raise ValidationError("The email address already exists")
+        return data.cleaned_data['email']
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from . import forms
 # Create your views here.
 def home(request):
     return render (request, 'home.html')
@@ -13,4 +14,5 @@ def driver_page(request):
     return render (request, 'home.html')
 
 def sign_up(request):
+    form = forms.SignUpForm()
     return render (request, 'sign-up.html')
