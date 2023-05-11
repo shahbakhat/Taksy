e83d164608ddf3615176c427092f2c5d11805f2b
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-11 09:23:53 +0100

    created the basic authetication for the user

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django .contrib.auth  import login
 from django.contrib.auth.decorators import login_required
 
 from . import forms
@@ -13,7 +14,21 @@ def passenger_page(request):
 def driver_page(request):
     return render (request, 'home.html')
 
+
 def sign_up(request):
     form = forms.SignUpForm()
-    return render (request, 'sign-up.html' ,{'form':form
-    })
+
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email'].lower()
+
+            user = form.save(commit=False)
+            user.username = email
+            user.save()
+
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'sign-up.html', {'form': form
+                                            })
