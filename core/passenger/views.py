6ebd12b880721be9b65c28dc01c139e8e2304781
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-15 16:57:26 +0100

    changed base.html and profile.html for update profie option for the passenger

--- core/passenger/views.py
+++ core/passenger/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
-
 from core.passenger import forms
 
 @login_required()
 def home(request):
-    return redirect(reverse('passenger.profile'))
+    return redirect(reverse('passenger:profile'))
 
 @login_required(login_url="/login/?next=/passenger/")
 def profile_page(request):
