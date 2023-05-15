693ad4bdffe8c4e50fb42351b57aba71561ec88d
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-15 20:15:24 +0100

    succesfully saved the user to the database and can as well access it throgh Django admin

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Passenger(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_avatar = models.ImageField(upload_to='passenger/avatar/', blank=True,null=True)
-    
-
+    user_avatar = models.ImageField(upload_to='passenger/avatar/', blank=True, null=True)
+    def __str__(self):
+        return self.user.get_full_name()
 
