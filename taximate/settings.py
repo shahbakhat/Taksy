e16af6d3722bea0a0b29fbdcf8b0812dc8a56131
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-15 20:40:08 +0100

    successfully managed the response nad request with middleware framework

--- taximate/settings.py
+++ taximate/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.ProfileMiddleware',
 ]
 
 ROOT_URLCONF = 'taximate.urls'
