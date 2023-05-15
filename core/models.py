381b1b5dabad8baecbf98050938653780aec49b6
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-15 19:47:35 +0100

    created user model and made migrations to the database

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Passenger(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_avatar = models.ImageField(upload_to='passenger/avatar/', blank=True,null=True)
+    
+
+
