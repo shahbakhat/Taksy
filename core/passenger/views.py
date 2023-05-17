a1d684bbbc56e15c18e48f8b53ed4bd96eaa348b
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-17 19:44:47 +0100

    created change password form and saved it succesfully in the model

--- core/passenger/views.py
+++ core/passenger/views.py
@@ -3,29 +3,49 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from core.passenger import forms
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+
+
 @login_required(login_url="/login/?next=/passenger/")
 def home(request):
     return redirect(reverse('passenger:profile'))
 
+
 @login_required(login_url="/login/?next=/passenger/")
 def profile_page(request):
-    user_form = forms.BasicUserForm
+    user_form = forms.BasicUserForm(instance=request.user)
     passenger_form = forms.BasicCustomerForm(instance=request.user.passenger)
+    password_form = PasswordChangeForm(request.user)
 
     if request.method == "POST":
-         user_from = forms.BasicCustomerForm(request.POST, instance=request.user)
-         passenger_form = forms.BasicCustomerForm(request.POST, request.FILES, instance=request.user.passenger)
-         if user_from.is_valid() and passenger_form.is_valid():
-             user_from.save()
-             passenger_form.save()
 
-             #Profile update toast
-             messages.success(request,'Your profile has been updated')
-             return redirect(reverse('passenger:profile'))
+        if request.POST.get('action') == 'update_prfile':
+            user_form = forms.BasicUserForm(
+                request.POST, instance=request.user)
+            passenger_form = forms.BasicCustomerForm(
+                request.POST, request.FILES, instance=request.user.passenger)
+
+            if user_form.is_valid() and passenger_form.is_valid():
+                user_form.save()
+                passenger_form.save()
+
+            # Profile update toast
+                messages.success(request, 'Your profile has been updated')
+                return redirect(reverse('passenger:profile'))
+
+        elif request.POST.get('action') == 'update_password':
+            password_form = PasswordChangeForm(request.user, request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)
+
+                messages.success(request, 'Your password has been updated')
+                return redirect(reverse('passenger:profile'))
 
     return render(request, 'passenger/profile.html',
                   {"user_form": user_form,
                    "passenger_form": passenger_form,
+                   "password_form": password_form,
                    }
                   )
-
