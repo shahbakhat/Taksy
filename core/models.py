51bc4a4376e021bdab1d5452a280d4560c17c7cf
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-23 16:45:31 +0100

    added a phone phone submition form to the profile

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Passenger(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.ImageField(upload_to='passenger/pfoile-photos/', blank=True, null=True)
+    phone_number = models.CharField(max_length=50, blank=True)
     def __str__(self):
         return self.user.get_full_name()
 
--- core/passenger/forms.py
+++ core/passenger/forms.py
@@ -1,15 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import ModelForm
-
 from core.models import Passenger
 
 class BasicUserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', )
+        fields = ('first_name', 'last_name')
 
 class BasicCustomerForm(forms.ModelForm):
     class Meta:
         model = Passenger
-        fields = ('profile_photo', )
+        fields = ('profile_photo', 'phone_number' )
