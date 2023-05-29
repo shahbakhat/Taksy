15f4176d0430855a436f28ba68d35c76b60f0e86
Shah Bakhat <shahbakhat008@gmail.com>
2023-05-29 13:34:33 +0100

    created taxi trip and category model

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import  models
 # Register your models here.
 admin.site.register(models.Passenger)
+admin.site.register(models.TaxiCategory)
+admin.site.register(models.TripStatus)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+from django.utils import timezone
 
 
 # Create your models here.
@@ -12,4 +14,47 @@ class Passenger(models.Model):
     stripe_card_last4 = models.CharField(max_length=225, blank=True)
     def __str__(self):
         return self.user.get_full_name()
+
+# Taxi category models
+class TaxiCategory(models.Model):
+    slug = models.CharField(max_length=225, unique=True)
+    FIVE_SEATER = "5 Seater"
+    SEVEN_SEATER = "7 Seater"
+    EIGHT_SEATER = "7 Seater"
+    TAXI_SIZE = (
+        (FIVE_SEATER ,'5 Seater'),
+        (SEVEN_SEATER ,' 7 Seater'),
+        (EIGHT_SEATER ,' 8 Seater'),
+        )
+    name  = models.CharField(max_length=20, choices= TAXI_SIZE,default= FIVE_SEATER)
+
+    def __str__(self):
+        return self.name
+
+
+
+# Trip model
+
+class TripStatus(models.Model ):
+    BOOKING_STATUS = 'booking in proccess'
+    TAXI_ARRIVED = 'arrived'
+    PASSENGER_ONBOAD = 'onboard'
+    TRIP_COMPLETED = 'complete'
+    TRIP_CANCELLED = 'cancelled'
+    STATUSES = (
+        (BOOKING_STATUS,'Booking in process'),
+        (TAXI_ARRIVED,'arrived'),
+        (PASSENGER_ONBOAD , 'Passenger on board'),
+        (TRIP_CANCELLED , 'Trip has been canceled'),
+        (TRIP_COMPLETED , 'Trip has been completed')
+
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Passenger, on_delete=models.CASCADE )
+    name = models.CharField(max_length=225)
+    status = models.CharField(max_length=20, choices= STATUSES,default= BOOKING_STATUS)
+    created_at = models.DateTimeField(default= timezone.now )
+    def __str__(self):
+            return self.name
+
 
