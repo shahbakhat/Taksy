e52e226f88ac31ef1417d7c6bfdf46cf9f8aab4d
Shah Bakhat <shahbakhat008@gmail.com>
2023-10-12 15:18:46 +0100

    fixed cors headers

--- taximate/settings.py
+++ taximate/settings.py
@@ -45,8 +45,9 @@ CSRF_COOKIE_HTTPONLY = True  # Recommended for security
 ALLOWED_HOSTS = ["taksy-083499a57331.herokuapp.com/","localhost"]
 # settings.py
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = ["https://taksy-083499a57331.herokuapp.com"]
+CORS_ALLOWED_ORIGINS = [
+    'https://taksy-083499a57331.herokuapp.com',
+]
 
 
 
