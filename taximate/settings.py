0381c4764bade56640c9d5d11a7186de558c9b32
Shah Bakhat <shahbakhat008@gmail.com>
2023-10-12 15:56:22 +0100

    FIXED SPEELING MISTAKE IN csrf

--- taximate/settings.py
+++ taximate/settings.py
@@ -38,7 +38,7 @@ GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-RF_TRUSTED_ORIGINS = ['https://taksy-083499a57331.herokuapp.com/login/']
+CSRF_TRUSTED_ORIGINS = ['https://taksy-083499a57331.herokuapp.com/login/']
 
 
 CORS_ALLOWED_ORIGINS = [
